b81d656d3a9b063e09faa34b7d8f3ea9fde1d4a9
MetehanKundak <mkundak1@binghamton.edu>
2021-10-01 21:45:09 -0400

    new Commit

--- app/src/main/java/com/example/cs441_mobilegamedevhw03/MainActivity.java
+++ app/src/main/java/com/example/cs441_mobilegamedevhw03/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.cs441_mobilegamedevhw03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
